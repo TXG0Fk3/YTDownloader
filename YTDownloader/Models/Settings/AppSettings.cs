e8f648c3ea0e938cd873ed504294ff25f408a5d2
TXG0Fk3 <txg0fk3@proton.me>
2026-06-28 15:57:30 -0300

    feat(Models/AppSettings): add MaxConcurrentPlaylistVideoInfoFetches and MaxConcurrentDownloads options

--- YTDownloader/Models/Settings/AppSettings.cs
+++ YTDownloader/Models/Settings/AppSettings.cs
@@ -9,6 +9,9 @@ public class AppSettings
     public WindowState MainWindowState { get; set; } = new();
     public ThemeMode Theme { get; set; } = ThemeMode.System;
 
+    public int MaxConcurrentPlaylistVideoInfoFetches { get; set; } = 8;
+    public int MaxConcurrentDownloads { get; set; } = 2;
+
     public string DefaultDownloadsPath { get; set; } =
         Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
     public bool AlwaysAskWhereSave { get; set; } = true;
